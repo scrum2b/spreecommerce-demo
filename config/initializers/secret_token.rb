408ed0f15cf8f6d8ef3d6989b91c573bf0e212ad
Bui Thanh Hai <hai.bth@gmail.com>
2013-08-31 01:44:51 +0700

    init project with Spree and demo data

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spree2Shipping::Application.config.secret_token = 'd49560f2505b9f862c713c6ee5aa47798f6a66a4e8abb0cba83aea0a7c6b87d687de91095b17f546534b880f1541960edfd481d9b3b3630cd372a349cce74416'
