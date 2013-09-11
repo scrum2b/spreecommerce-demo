96263c3cb3be37dcaf6db595b73b032875e3abd5
Bui Thanh Hai <hai.bth@gmail.com>
2013-09-11 20:54:45 +0700

    Fix memcache config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,8 @@ Spree2Package::Application.configure do
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
+  config.cache_store = ENV["MEMCACHIER_SERVERS"].split(","),
+                       {:username => ENV["MEMCACHIER_USERNAME"], :password => ENV["MEMCACHIER_PASSWORD"]}
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
